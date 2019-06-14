9e7e460c597eb29dd88d218ad872100ff3049568
Conor Walsh <conorwalsh0703@gmail.com>
2019-06-14 18:51:00 +0100

    conected to mongoDB

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'theTopShelf'
+app.config["MONGO_URI"] = 'mongodb+srv://root:jk6200dl@myfirstcluster-fdipx.mongodb.net/theTopShelf?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def home():
-    return "Hello World!"
+@app.route('/categories')
+def categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())
+
+
 
 if __name__ == "__main__":
     app.run(host = os.environ.get("IP"),
