856dc459713f6c86d5058244e8fcdc517fad027c
Conor Walsh <conorwalsh0703@gmail.com>
2019-06-21 18:42:59 +0100

    created add a cocktail functionality

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ def categories():
 @app.route('/categories/<category_id>')
 def category_id(category_id):
     the_category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
-    all_cocktails = mongo.db.cocktails.find()
-    all_categories = mongo.db.categories.find()
     return render_template("drink.html",categories=mongo.db.categories.find(), category = the_category, cocktails = mongo.db.cocktails.find())
 
 # @app.route('/categories/<category_id>/<cocktail_id>')
@@ -38,6 +36,16 @@ def cocktail_id(cocktail_id):
     
 
     return render_template("single.html", cocktail = cocktail )
+
+@app.route('/add_cocktail')
+def add_cocktail():
+    return render_template('addcocktail.html', categories=mongo.db.categories.find())
+
+@app.route('/insert_cocktail', methods=['POST'])
+def insert_cocktail():
+    cocktails = mongo.db.cocktails
+    cocktails.insert_one(request.form.to_dict()) 
+    return redirect(url_for('categories'))
 
 
 if __name__ == "__main__":
---
+++ static/js/form.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    var ingredientID = 1;
+
+    $('#add_ing').click(function (e) {
+        e.preventDefault();
+        $('#ingredients-list').append(`<div class="input-field col s6">
+        <input type="text" id="ingredient_name` + ingredientID + `" name="ingredient_name` + ingredientID + `" class="materialize-textarea">
+        <label for="ingredient_name` + ingredientID + `">Ingredient</label>
+    </div>
+    <div class="input-field col s6">
+        <input type="text" id="ingredient_amount` + ingredientID + `" name="ingredient_amount` + ingredientID + `" class="materialize-textarea">
+        <label for="ingredient_amount` + ingredientID + `">Amount</label>
+    </div>`);
+
+        ingredientID++;
+
+        if (ingredientID > 1) {
+            $("#remove_ing").show();
+        }
+    });
+
+    $('#remove_ing').click(function (e) {
+        e.preventDefault();
+        $("#ingredients-list  > div").slice(-2).remove();
+        ingredientID--;
+    });
+
+
+
+
+    var methodID = 1;
+
+    $('#add_step').click(function (e) {
+        e.preventDefault();
+        $('#method-list').append(`
+    <div class="input-field col s12">
+        <input type="text" id="method` + methodID + `" name="method` + methodID + `" class="materialize-textarea">
+        <label for="method` + methodID + `">Enter step details</label>
+    </div>`);
+
+        methodID++;
+
+        if (methodID > 1) {
+            $("#remove_step").show();
+        }
+    });
+
+    $('#remove_step').click(function (e) {
+        e.preventDefault();
+        $("#method-list  > div").slice(-2).remove();
+        methodID--;
+    });
+
+
+});
