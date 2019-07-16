4565c2bf519cea6eb2fad0e1b846e38562f2a0f2
Conor Walsh <conorwalsh0703@gmail.com>
2019-07-16 20:29:10 +0100

    add environment variables

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 MONGO_DBNAME = 'theTopShelf'
-MONGO_URI ="mongodb+srv://root:jk6200dl@myfirstcluster-fdipx.mongodb.net/theTopShelf?retryWrites=true&w=majority"
-
+MONGO_URI =os.environ.get("MONGO_URI")
 SECRET_KEY  = os.urandom(32)
 
 
